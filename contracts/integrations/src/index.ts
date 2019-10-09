181a155b0075bb658f501e9651a5bfbc044ad4a5
Alex Towle <jalextowle@gmail.com>
2019-10-09 10:25:01 -0700

    `@0x:contract-integrations` Added the integrations package to CI

--- contracts/integrations/src/index.ts
+++ contracts/integrations/src/index.ts
@@ -1,5 +1,6 @@
 export * from './artifacts';
 export * from './wrappers';
+export * from './cache';
 export * from './function_assertions';
 export * from '../test/utils/deployment_manager';
 export * from '../test/utils/address_manager';
